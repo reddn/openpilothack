e4b3cd4517b065222348579f73efeda859e5b6f8
Thomas S <thomas@reddn.com>
2018-12-18 00:54:30 -0500

    added radaroffcan=False

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -232,6 +232,7 @@ class CarInterface(object):
       ret.longitudinalKpV = [1.2, 0.8, 0.5]
       ret.longitudinalKiBP = [0., 35.]
       ret.longitudinalKiV = [0.18, 0.12]
+      ret.radarOffCan = False 
 
     elif candidate == CAR.ACURA_ILX:
       stop_and_go = False
